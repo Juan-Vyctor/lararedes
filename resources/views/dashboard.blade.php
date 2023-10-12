f23cd4c85335f4311a164c67623ae0c1622c50dc
Juan-Vyctor <juanvyctor.sgo@gmail.com>
2023-10-12 09:27:37 -0300

    arrumado problema de rotas

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -196,8 +196,8 @@ integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUU
             ?>
             <p class="lead">O que você deseja fazer?</p>
             <p class="lead">
-            <a href="" class="btn btn-lg btn-light fw-bold border-white bg-white">Sair da conta</a>
-            <a href="" class="btn btn-lg btn-light fw-bold border-white bg-white">Sair da conta só que 2</a>
+            <a href="/" class="btn btn-lg btn-light fw-bold border-white bg-white">Sair da conta</a>
+            <a href="/" class="btn btn-lg btn-light fw-bold border-white bg-white">Sair da conta só que 2</a>
             <a href="/" class="btn btn-lg btn-light fw-bold border-white bg-white">Sairsó que 5</a>
             </p>
     </main>
