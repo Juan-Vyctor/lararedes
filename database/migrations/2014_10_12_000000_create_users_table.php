ff5af352866bb9b239761958b5490b1f16e8da4a
Juan-Vyctor <juanvyctor.sgo@gmail.com>
2023-10-12 12:30:37 -0300

    migrate

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,11 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->id('usu_id');
+            $table->string('usu_nome');
+            $table->string('usu_email')->unique();
+            $table->string('usu_senha');
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
